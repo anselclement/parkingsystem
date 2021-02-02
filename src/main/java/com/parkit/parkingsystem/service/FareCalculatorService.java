0cbfc3783c6aae2c0e2f96282cb8f20f0e46eb43
Clement Ansel <clem-nogare@hotmail.fr>
2021-02-02 11:00:38 +0100

    correction de la logique pour avoir le bon temps passé en stationnement, ajout aussi de la logique pour temps moins de 30min et utilisateur régulier(commit en retard)

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -1,31 +1,59 @@
 package com.parkit.parkingsystem.service;
 
 import com.parkit.parkingsystem.constants.Fare;
+import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.model.Ticket;
+import com.parkit.parkingsystem.util.InputReaderUtil;
 
 public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket){
+    //TODO :parcours la liste retourne true si valeur plaque rentrer = true
+    public boolean calculateFivePercentReduction() throws Exception {
+
+        TicketDAO ticketDAO = new TicketDAO();
+        InputReaderUtil inputReaderUtil = new InputReaderUtil();
+
+        return ticketDAO.getVehicleRegNumberInTheDataBase().contains(inputReaderUtil.readVehicleRegistrationNumber());
+    }
+
+
+    public void calculateFare(Ticket ticket) throws Exception {
         if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+        //int inHour  ticket.getInTime().getHours();
+        //int outHour  ticket.getOutTime().getHours();
+
+        Double inHour = (double)ticket.getInTime().getTime();
+        Double outHour = (double)ticket.getOutTime().getTime();
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+        //int duration = outHour - inHour;
+        double duration = Math.round((((outHour - inHour)/60/1000)/60)*100.0)/100.0;
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                if(duration <= 0.5){
+                    ticket.setPrice(0);
+                }else if (calculateFivePercentReduction()){ //TODO : -5% calcul
+                    ticket.setPrice((duration * Fare.CAR_RATE_PER_HOUR) * Fare.REDUCTION_FIVE_PERCENT);
+                }else{
+                    ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                }
                 break;
             }
             case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+                if(duration <= 0.5){
+                    ticket.setPrice(0);
+                }else if (calculateFivePercentReduction()) { //TODO : -5% calcul
+                    ticket.setPrice((duration * Fare.BIKE_RATE_PER_HOUR) * Fare.REDUCTION_FIVE_PERCENT);
+                }else {
+                    ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+                }
                 break;
             }
-            default: throw new IllegalArgumentException("Unkown Parking Type");
+            default: throw new IllegalArgumentException("Unknown Parking Type");
         }
     }
 }
